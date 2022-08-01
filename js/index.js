d32b6045f63d65ce030cd1e020566d7598e89620
Wwwhisperr <96454062+Wwwhisperr@users.noreply.github.com>
2022-08-01 09:25:39 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
-var header=document.querySelector('header');
-var nav=header.querySelector('.nav');
-var banner=header.querySelector('.banner');
-window.onscroll=function(){
-console.log(document.documentElement.scrollTop);
-if(document.documentElement.scrollTop>70){
-    nav.className='navafter';
-    banner.style.transform='translateY(50px)';
-}else {
-    nav.className='nav';
-    banner.style.transform='translateY(0)';
-}
+window.onload = function () {
+    var header = document.querySelector('header');
+    var nav = header.querySelector('.nav');
+    var banner = header.querySelector('.banner');
+    window.onscroll = function () {
+        console.log(document.documentElement.scrollTop);
+        if (document.documentElement.scrollTop > 70) {
+            nav.className = 'navafter';
+            banner.style.transform = 'translateY(50px)';
+        } else {
+            nav.className = 'nav';
+            banner.style.transform = 'translateY(0)';
+        }
+    }
 }
